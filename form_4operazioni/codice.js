c34f6647525a4fde3eafb5041b2847df49ca681c
laboint20 <81230672+laboint20@users.noreply.github.com>
2024-04-22 09:39:31 +0200

    Add files via upload

---
+++ form_4operazioni/codice.js
@@ -0,0 +1,62 @@
+
+// Recupera i riferimenti agli elementi HTML per i due campi di input 'txt_a' e 'txt_b'.
+let a = document.getElementById('txt_a');
+let b = document.getElementById('txt_b');
+// Questa funzione imposta il focus sul campo di input 'a'.
+
+function canc() {
+    a.focus();
+}
+// Questa funzione imposta il focus sul campo di input 'txt_a' quando la finestra si carica completamente.
+window.onload = a.focus();
+// Questa funzione esegue i calcoli quando l'utente preme il pulsante di calcolo.
+function calcola() {
+    // Recupera i valori inseriti nei campi di input e li converte in numeri.
+    let num1 = parseFloat(a.value);
+    let num2 = parseFloat(b.value);
+
+    // Recupera l'elemento HTML dove mostrare il risultato.
+    let risultato = document.getElementById('txt_risultato');
+
+    // definisce il pattern da controllare con la regular expression.
+    let pattern=/^[0-9]+(\.[0-9]+)?$/;
+    // Verifica se il primo numero è valido.     
+        if (a.value.search(pattern) == -1){
+        // Mostra un messaggio di avviso e reimposta il focus sul campo 'txt_a'.
+        alert("Inserisci il primo valore");
+        a.focus();
+        a.value = ""; // Cancella il contenuto del campo 'txt_a'.
+        return false; // Interrompe l'esecuzione della funzione.
+    }
+
+    // Verifica se il secondo numero è valido.
+          
+        if (b.value.search(pattern) == -1){
+        // Mostra un messaggio di avviso e reimposta il focus sul campo 'txt_b'.
+        alert("Inserisci il secondo valore");
+        b.focus();
+        b.value = ""; // Cancella il contenuto del campo 'txt_b'.
+        return false; // Interrompe l'esecuzione della funzione.
+    }
+
+    // Recupera l'operazione selezionata dall'utente.
+    let operando = document.querySelector('select').selectedIndex;
+
+    // Esegue il calcolo in base all'operazione selezionata.
+    switch (operando) {
+        case 0:
+            break; // Selezionata l'opzione "Seleziona operazione", non fare nulla.
+        case 1:
+            risultato.value = num1 + num2; // Somma i due numeri.
+            break;
+        case 2:
+            risultato.value = num1 - num2; // Sottrae il secondo numero dal primo.
+            break;
+        case 3:
+            risultato.value = num1 / num2; // Divide il primo numero per il secondo.
+            break;
+        case 4:
+            risultato.value = num1 * num2; // Moltiplica i due numeri.
+            break;
+    }
+}
